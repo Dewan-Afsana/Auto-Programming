a99b4f54e14c9e9ae9da92802a5de243ffb9ce91
riaf <dewan.afsana@shopup.org>
2023-01-07 01:24:43 +0600

    second commit

---
+++ src/Prime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner rd = new Scanner(System.in);
+        System.out.print("From keybord: ");
+        String var = rd.next();
+        if (var.compareTo("afsan") == 0) {
+            System.out.println("yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
